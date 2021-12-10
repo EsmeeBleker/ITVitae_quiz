d3039b2aad07dd0588776d409b43a98f912f6ed6
gerrit <gerritvanloenen@gmail.com>
2021-12-10 12:36:40 +0100

    Volgorde van de antwoorden is gerandomiseerd

--- src/main/java/Model/Antwoord.java
+++ src/main/java/Model/Antwoord.java
@@ -1,6 +1,24 @@
 package Model;
 
 public class Antwoord {
-    public String antwoord;
-    public boolean correct;
+    private String antwoord;
+    private boolean correct;
+
+    public boolean getCorrect(){
+        return this.correct;
+    }
+    public void setCorrect(boolean correct) {
+        this.correct = correct;
+    }
+
+    public void setAntwoord(String antwoord) {
+        this.antwoord = antwoord;
+    }
+    public String getAntwoord() {
+        return this.antwoord;
+    }
+
+    public Antwoord getObject() {
+        return this;
+    }
 }
